41eccde8e2fd6e4ae9f99a2bca1eaea82da53c31
Murzin Andrey <MVMurzin@sberbank.ru>
2021-10-29 14:24:39 +0300

    init commit

---
+++ app/src/main/java/andrey/murzin/com/kinolab/MainActivity.kt
@@ -0,0 +1,11 @@
+package andrey.murzin.com.kinolab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
