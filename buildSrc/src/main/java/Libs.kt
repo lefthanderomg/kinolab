f7a62ed8a1ded953498f6afd2bd92f86e47f38ad
Murzin Andrey <MVMurzin@sberbank.ru>
2021-10-30 10:44:32 +0300

    fix build failed

--- buildSrc/src/main/java/Libs.kt
+++ buildSrc/src/main/java/Libs.kt
@@ -1,21 +1,21 @@
 object Libs {
 
     object Core {
-        val kotlinVersion = "1.5.31"
-        val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
-        val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
-        val buildGradle = "com.android.tools.build:gradle:7.0.3"
+        private const val kotlinVersion = "1.5.31"
+        const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
+        const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
+        const val buildGradle = "com.android.tools.build:gradle:7.0.3"
     }
 
     object Android {
-        val coreKtx = "androidx.core:core-ktx:1.7.0"
-        val appCompat = "androidx.appcompat:appcompat:1.3.1"
-        val material = "com.google.android.material:material:1.4.0"
+        const val coreKtx = "androidx.core:core-ktx:1.7.0"
+        const val appCompat = "androidx.appcompat:appcompat:1.3.1"
+        const val material = "com.google.android.material:material:1.4.0"
     }
 
     object Test {
-        val junit = "junit:junit:4.+"
-        val androidJunit = "androidx.test.ext:junit:1.1.3"
-        val espressoCore = "androidx.test.espresso:espresso-core:3.4.0"
+        const val junit = "junit:junit:4.13.2"
+        const val androidJunit = "androidx.test.ext:junit:1.1.3"
+        const val espressoCore = "androidx.test.espresso:espresso-core:3.4.0"
     }
 }
